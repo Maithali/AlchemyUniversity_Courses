ead16add1c85a2c28ffd647e396100f9d1ad93ea
Maithali <ghardem1@gmail.com>
2025-06-04 13:27:38 +0530

    new task

---
+++ Javascript/Js_Fundamentals/CreatePizzaOrder.js
@@ -0,0 +1,18 @@
+/*
+Let's create an object representing a pizza order!
+
+In the order object, add the following three keys with values accordingly:
+
+    pizzas - Any number greater than zero.
+    extraCheese - A boolean. Either true or false.
+    deliveryInstructions - Any string of instructions.
+
+    Feel free to get creative with these values! So long as the values are the approriate type, the test cases will pass!
+*/
+const order = {
+    pizzas:10,
+    extraCheese:true,
+    deliveryInstructions : "Give it to the Guard",
+};
+
+module.exports = order;
